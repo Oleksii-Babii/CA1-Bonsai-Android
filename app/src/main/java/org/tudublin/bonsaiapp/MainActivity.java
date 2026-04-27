d8ab5514c2027dc18f3fbdc3b77cf133f9360d98
ihor.tryndey <igortryndey@gmail.com>
2026-04-27 09:30:51 +0100

    feat(home): wire home screen buttons (browse species / my collection)

--- app/src/main/java/org/tudublin/bonsaiapp/MainActivity.java
+++ app/src/main/java/org/tudublin/bonsaiapp/MainActivity.java
@@ -1,12 +1,26 @@
 package org.tudublin.bonsaiapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button browseSpecies = findViewById(R.id.btnViewSpecies);
+        Button myCollection  = findViewById(R.id.btnViewTrees);
+
+        browseSpecies.setOnClickListener(v ->
+                startActivity(new Intent(MainActivity.this, SpeciesListActivity.class)));
+
+        myCollection.setOnClickListener(v -> {
+            // TODO: open trees screen once it lands
+        });
     }
 }
