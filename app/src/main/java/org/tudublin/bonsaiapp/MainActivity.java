bcdeb5e6ae84906148258ddcc4ce20e624cf0c5d
Oleksii-Babii <babiy.olexiy@ukr.net>
2026-04-30 16:38:09 +0100

    feat(home): featured species hero with Glide-loaded image and chip

--- app/src/main/java/org/tudublin/bonsaiapp/MainActivity.java
+++ app/src/main/java/org/tudublin/bonsaiapp/MainActivity.java
@@ -2,25 +2,71 @@ package org.tudublin.bonsaiapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
+import org.tudublin.bonsaiapp.api.RetrofitClient;
+import org.tudublin.bonsaiapp.model.Species;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "BonsaiApp";
+
+    private ImageView imageFeatured;
+    private TextView textFeaturedName, textFeaturedOrigin, textFeaturedDifficulty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageFeatured          = findViewById(R.id.imageFeatured);
+        textFeaturedName       = findViewById(R.id.textFeaturedName);
+        textFeaturedOrigin     = findViewById(R.id.textFeaturedOrigin);
+        textFeaturedDifficulty = findViewById(R.id.textFeaturedDifficulty);
+
         Button browseSpecies = findViewById(R.id.btnViewSpecies);
         Button myCollection  = findViewById(R.id.btnViewTrees);
-
         browseSpecies.setOnClickListener(v ->
                 startActivity(new Intent(MainActivity.this, SpeciesListActivity.class)));
+        myCollection.setOnClickListener(v -> { /* trees screen lands later */ });
 
-        myCollection.setOnClickListener(v -> {
-            // TODO: open trees screen once it lands
+        loadFeaturedSpecies();
+    }
+
+    private void loadFeaturedSpecies() {
+        RetrofitClient.getService().getAllSpecies().enqueue(new Callback<List<Species>>() {
+            @Override
+            public void onResponse(Call<List<Species>> call, Response<List<Species>> response) {
+                if (response.isSuccessful() && response.body() != null && !response.body().isEmpty()) {
+                    Species s = response.body().get(0);
+                    textFeaturedName.setText(s.getName());
+                    textFeaturedOrigin.setText(s.getOriginCountry());
+                    textFeaturedDifficulty.setText(s.getDifficultyLevel());
+                    if (!TextUtils.isEmpty(s.getImageUrl())) {
+                        Glide.with(MainActivity.this).load(s.getImageUrl()).into(imageFeatured);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Species>> call, Throwable t) {
+                Log.e(TAG, "Featured species failed: " + t.getMessage());
+            }
         });
     }
 }
